c2c79b3c86880144a0694acc082c5c0557e7b839
s.ditaranto <simodita99@gmail.com>
2024-02-07 16:01:36 +0100

    livelli difficoltà aggiunti, bonus completato

--- js/script.js
+++ js/script.js
@@ -3,23 +3,60 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
-
+// Bonus
+// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+/*_______________________________________________________________________________________________________________________ */
 
 // memorizzo il bottone "genera" che al click farà visualizzare la griglia
 buttonStartElement = document.querySelector("#start");
 
-buttonStartElement.addEventListener("click", 
+// BONUS
+// memorizzo in una variabile la difficoltà scelta dall'utente
+let difficultyElement = document.querySelector("#choice");
+
+
+buttonStartElement.addEventListener("click",
+
 
     function() {
+        // memorizzo l'elemento griglia ("grid") 
+        const gridElement = document.querySelector("#grid");
+        // ad ogni pressione del button, il contenuto della griglia si deve azzerare
+        gridElement.innerHTML = "";
+
+
+        // in base alla scelta dell'utente decido quante celle devo visualizzare in pagina
+        // il valore sarà il numero di iterazioni del ciclo for
+        let userDifficulty = 100;
+        // valore di default
+        if(difficultyElement.value == 2) {
+
+            userDifficulty = 81;
+            document.querySelector(".container").className = "container medium";
+            // document.querySelector(".container").style.width = "900px";
+
+        } else if(difficultyElement.value == 3) {
+
+            userDifficulty = 49;
+            document.querySelector(".container").className = "container easy";
+            // document.querySelector(".container").style.width = "700px";
+
+        } else {
+
+            document.querySelector(".container").className = "container";
+            // document.querySelector(".container").style.width = "1000px";
+
+        }
 
         // prendiamo la griglia 
         // utilizzando un ciclo for, per ogni iterazione creiamo una cella nella griglia e inseriamo come testo
-        // l'indice dell'iterazione attuale
-        
-        const gridElement = document.querySelector("#grid");
-        
-        // griglia 10x10
-        for(let i = 0; i < 100; i++) {
+        // l'indice dell'iterazione attuale 
+
+        // creazione griglia "userDifficulty" x "userDifficulty"
+        for(let i = 0; i < userDifficulty; i++) {
         
             // creiamo l'elemtno
             const newElement = document.createElement("div");
